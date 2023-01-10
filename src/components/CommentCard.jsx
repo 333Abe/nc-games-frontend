0315e7b1c081bcfd6d8295283db14ca629a706de
333Abe <adrianbennett333@gmail.com>
2023-01-10 15:26:12 +0000

    individual reviews comments now visible

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,5 +1,11 @@
-const CommentCard = () => {
-  return <p>this is a comment card</p>;
+const CommentCard = ({ author, body, votes }) => {
+  return (
+    <div className="CommentCard">
+      <h3>{author} says:</h3>
+      <p>{body}</p>
+      <p>votes: {votes}</p>
+    </div>
+  );
 };
 
 export default CommentCard;
